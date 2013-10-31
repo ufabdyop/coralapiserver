3bcebbb2471f71f7128556c00b408a5026cb5cb6
Ryan Taylor <ryant@nanofab.utah.edu>
2013-10-31 15:18:47 -0600

    Can successfully look up a member in coral

---
+++ src/main/java/edu/utah/nanofab/coralapiserver/resources/CoralApiMemberResource.java
@@ -0,0 +1,53 @@
+package edu.utah.nanofab.coralapiserver.resources;
+
+import edu.nanofab.coralapi.CoralServices;
+import edu.nanofab.coralapi.resource.Member;
+import edu.utah.nanofab.coralapiserver.core.Saying;
+import org.slf4j.Logger;
+
+import com.google.common.base.Optional;
+import com.yammer.metrics.annotation.Timed;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Path("/member")
+@Produces(MediaType.APPLICATION_JSON)
+public class CoralApiMemberResource {
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+    public static final Logger logger = LoggerFactory.getLogger(CoralApiMemberResource.class);
+
+    public CoralApiMemberResource(String template, String defaultName) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+    }
+
+    @GET
+    @Timed
+    public Member member(@QueryParam("name") Optional<String> name) {
+    	Member fetchedMember = new Member();
+		try {
+			logger.debug("Will look up member in coral");
+	    	if (name.isPresent()) {
+	    		logger.debug("name is present and is " + name.get());
+	    		CoralServices api = new CoralServices();
+	    		logger.debug("coral api instantiated");
+				fetchedMember = api.getMember(name.get());
+	    		logger.debug("member fetched");
+	    	}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return fetchedMember;
+    }
+}
