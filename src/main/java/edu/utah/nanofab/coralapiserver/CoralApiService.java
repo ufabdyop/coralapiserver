cc76b79bcc9bf66383377de132b5bd127f2a116a
Ryan Taylor <ryant@nanofab.utah.edu>
2013-10-31 15:19:04 -0600

    Can successfully look up a member in coral

--- src/main/java/edu/utah/nanofab/coralapiserver/CoralApiService.java
+++ src/main/java/edu/utah/nanofab/coralapiserver/CoralApiService.java
@@ -1,7 +1,9 @@
 package edu.utah.nanofab.coralapiserver;
 
+import edu.utah.nanofab.coralapiserver.resources.CoralApiMemberResource;
 import edu.utah.nanofab.coralapiserver.resources.CoralApiResource;
 import edu.utah.nanofab.coralapiserver.health.TemplateHealthCheck;
+
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
@@ -22,6 +24,7 @@ public class CoralApiService extends Service<CoralApiConfiguration> {
 		final String template = configuration.getTemplate();
 		final String defaultName = configuration.getDefaultName();
 		environment.addResource(new CoralApiResource(template, defaultName));
+		environment.addResource(new CoralApiMemberResource(template, defaultName));
 		environment.addHealthCheck(new TemplateHealthCheck(template));
     }
 
