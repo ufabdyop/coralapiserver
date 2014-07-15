646614b908823555d6e947c70daf85ab9ffe6cad
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2014-07-15 10:08:54 -0600

    Added log messages to the PasswordResetOperation and changed the log level in the YAML configuration file.

--- src/main/java/edu/utah/nanofab/coralapiserver/resources/operations/PasswordResetOperationPost.java
+++ src/main/java/edu/utah/nanofab/coralapiserver/resources/operations/PasswordResetOperationPost.java
@@ -1,10 +1,15 @@
 package edu.utah.nanofab.coralapiserver.resources.operations;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.utah.nanofab.coralapiserver.core.PasswordResetRequest;
 
 
 public class PasswordResetOperationPost extends ResourceOperation {
  
+	public static final Logger logger = LoggerFactory.getLogger(PasswordResetOperationPost.class);
+	
 	@Override
 	public void performOperationImpl() throws Exception {
 		PasswordResetRequest request = (PasswordResetRequest) this.postedObject.get();
@@ -12,8 +17,11 @@ public class PasswordResetOperationPost extends ResourceOperation {
 		String newPass = request.getPassword();
 		
 		try {
+			logger.debug("Updating password for user '" + memberName + "' with new password '" + newPass + "'");
 			this.api.updatePassword(memberName, newPass);
 		} catch(Exception e) {
+			logger.error("Error: Could not update password for user '" + memberName + "'");
+			logger.trace(e.getMessage(), e);
 			this.setReturnValue(false);
 			return;
 		}
