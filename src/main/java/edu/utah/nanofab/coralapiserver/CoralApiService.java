afa91151006030a1bde3dd9e594ad479b549a96c
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-04 13:13:01 -0700

    add whoami resource

--- src/main/java/edu/utah/nanofab/coralapiserver/CoralApiService.java
+++ src/main/java/edu/utah/nanofab/coralapiserver/CoralApiService.java
@@ -7,7 +7,6 @@ import io.dropwizard.auth.basic.BasicAuthProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import io.dropwizard.assets.AssetsBundle;
-
 import edu.utah.nanofab.coralapiserver.auth.SimpleAuthenticator;
 import edu.utah.nanofab.coralapiserver.auth.User;
 import edu.utah.nanofab.coralapiserver.resources.CoralApiAccountResource;
@@ -19,6 +18,7 @@ import edu.utah.nanofab.coralapiserver.resources.CoralApiProjectMembershipResour
 import edu.utah.nanofab.coralapiserver.resources.CoralApiProjectResource;
 import edu.utah.nanofab.coralapiserver.resources.CoralApiProjectsResource;
 import edu.utah.nanofab.coralapiserver.resources.CoralApiVersionResource;
+import edu.utah.nanofab.coralapiserver.resources.CoralApiWhoAmIResource;
 
 
 public class CoralApiService extends Application<CoralApiConfiguration> {
@@ -41,6 +41,7 @@ public class CoralApiService extends Application<CoralApiConfiguration> {
 
       environment.jersey().register(new BasicAuthProvider<User>(new SimpleAuthenticator(tokens, sessionTokens, coralIor, coralConfigUrl ), "REALM STRING"));
       environment.jersey().register(new CoralApiVersionResource());
+      environment.jersey().register(new CoralApiWhoAmIResource());
       environment.jersey().register(new CoralApiAuthTokenResource(coralIor, coralConfigUrl, sessionTokens));
       environment.jersey().register(new CoralApiMemberResource(coralIor, coralConfigUrl));
       environment.jersey().register(new CoralApiLabRoleResource(coralIor, coralConfigUrl));
---
+++ src/main/java/edu/utah/nanofab/coralapiserver/resources/CoralApiWhoAmIResource.java
@@ -0,0 +1,26 @@
+package edu.utah.nanofab.coralapiserver.resources;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+import io.dropwizard.auth.Auth;
+
+import com.codahale.metrics.annotation.Timed;
+
+import edu.utah.nanofab.coralapiserver.auth.User;
+
+@Path("/whoami")
+@Produces(MediaType.APPLICATION_JSON)
+public class CoralApiWhoAmIResource {
+  
+  @GET
+  @Timed
+  public String getRequest(@Auth User user) {
+	  return user.getUsername();
+  }  
+  
+}
