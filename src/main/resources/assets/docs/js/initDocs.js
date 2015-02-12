17407d88ec0266c1bb5aa52b7409cf4cd3378837
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-12 15:04:41 -0700

    add more examples to docs

--- src/main/resources/assets/docs/js/initDocs.js
+++ src/main/resources/assets/docs/js/initDocs.js
@@ -1,4 +1,12 @@
-var apiUrl = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port ;
+var port = window.location.port;
+var portOverride=window.location.href.split('#')[1];
+if (typeof portOverride != 'undefined' ) {
+  port = portOverride;
+}
+var versionPrefix = '/v0';
+var apiUrl = window.location.protocol + '//' + window.location.hostname + ':' + port + versionPrefix;
+
+//port override
 
 $('#apiUrl').val(apiUrl);
 $('#currentApi').html(apiUrl);
--- src/main/resources/assets/docs/js/samples.js
+++ src/main/resources/assets/docs/js/samples.js
@@ -1,26 +1,53 @@
-function getVersion (coralApiUrl) {
-  $.ajax({
-    url: coralApiUrl + "/version",
-    type: "GET"
-  }).success(function(data, code) {
-    alert("Version is " + data.versionNumber);
-  }).error(function(data, code) {
-    alert(" Error " + data);
-  });
+function getVersion(coralApiUrl) {
+  var request = new XMLHttpRequest();
+  request.open("GET", coralApiUrl + '/version', true);
+  request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          if (request.status === 200) {  
+              alert(request.responseText);
+          } else {
+              alert('ERROR: ' + request.statusText);
+          }
+        }
+  }
+  request.send(null);
 };
 
 function authenticate(coralApiUrl, username, password) {
-  $.ajax({
-    url: coralApiUrl + "/authenticate",
-    type: "GET",
-    beforeSend: function(xhr) {
-      xhr.setRequestHeader("Authorization",
-        "Basic " + btoa(username + ":" + password)
-        );
-    }
-  }).success(function(data, code) {
-    alert("Response is " + JSON.stringify(data));
-  }).error(function(data, code) {
-    alert(" Error " + JSON.stringify(data));
-  });
+  var request = new XMLHttpRequest();
+  request.open("GET", coralApiUrl + '/authenticate', true);
+  request.setRequestHeader("Accept", "application/json");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+
+  request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          if (request.status === 200) {
+              alert(request.responseText);
+          } else {
+              alert('ERROR: ' + request.statusText);
+          }
+        }
+  }
+  request.send(null);
+};
+
+function resetPassword(coralApiUrl, username, password, targetUsername, newPassword) {
+  var request = new XMLHttpRequest();
+  request.open("POST", coralApiUrl + '/resetPassword', true);
+  request.setRequestHeader("Accept", "application/json");
+  request.setRequestHeader("Content-Type", "application/json");
+  request.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+
+  request.onreadystatechange = function() {
+        if (request.readyState == 4) {
+          if (request.status === 200) {
+              alert(request.responseText);
+          } else {
+              alert('ERROR: ' + request.statusText);
+          }
+        }
+  }
+  data = {"name" : targetUsername, "password" : newPassword};
+  request.send(JSON.stringify(data));
 };
