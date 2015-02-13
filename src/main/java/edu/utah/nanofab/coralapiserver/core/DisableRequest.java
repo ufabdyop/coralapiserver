08f4b5a41b47e3dbb5a8e1b021b7ea7e438dcebf
Ryan Taylor <ryant@nanofab.utah.edu>
2015-02-13 14:16:22 -0700

    remove member

--- src/main/java/edu/utah/nanofab/coralapiserver/core/DisableRequest.java
+++ src/main/java/edu/utah/nanofab/coralapiserver/core/DisableRequest.java
@@ -1,14 +1,7 @@
 package edu.utah.nanofab.coralapiserver.core;
 
 public class DisableRequest {
-  public String member;
   public String item;
-  public String getMember() {
-    return member;
-  }
-  public void setMember(String member) {
-    this.member = member;
-  }
   public String getItem() {
     return item;
   }
