b24939d51f4f353be0983d61888c14eaf9bd60da
ryant <ryant@nanofab.utah.edu>
2015-09-03 11:15:57 -0600

    bump version

--- util/swagger-helper.js
+++ util/swagger-helper.js
@@ -5,7 +5,7 @@ var host = '172.17.42.1';
 host = 'localhost';
 var port = 8080;
 port = 4001;
-var apiVersion = "0.3.5";
+var apiVersion = "0.3.7";
 var YAML = require('json2yaml');
 
 function getJson(page, callback) {
