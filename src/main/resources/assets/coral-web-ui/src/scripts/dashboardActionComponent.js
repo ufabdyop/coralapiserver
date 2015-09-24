ec61fb978c21297256fe8155b0114ef7bc01ddb9
ryant <ryant@nanofab.utah.edu>
2015-09-24 08:29:20 -0600

    first commit

---
+++ src/main/resources/assets/coral-web-ui/src/scripts/dashboardActionComponent.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+* props: thumbnail, header, description
+*/
+var DashboardAction = React.createClass({
+  render: function() {
+    return (
+      <div className="col-xs-6 col-sm-3 placeholder">
+        <img src={this.props.thumb} className="img-responsive" alt={this.props.header} />
+        <h4>{this.props.header}</h4>
+        <span className="text-muted">{this.props.description}</span>
+      </div>
+    );
+  }
+});
+
+export default DashboardAction;
